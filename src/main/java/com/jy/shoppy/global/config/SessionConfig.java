f733534c71725d3d0e4cdc419cf15d2275c556a4
test_git <test@gmail.com>
2025-12-12 19:41:26 +0900

    feat:redisTemplate 사용 세션 데이터 구조 테스트

--- src/main/java/com/jy/shoppy/global/config/SessionConfig.java
+++ src/main/java/com/jy/shoppy/global/config/SessionConfig.java
@@ -13,23 +13,6 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @Configuration
 @EnableRedisHttpSession
 public class SessionConfig {
-//    @Bean
-//    public GenericJackson2JsonRedisSerializer springSessionDefaultRedisSerializer() {
-//        ObjectMapper objectMapper = new ObjectMapper();
-//
-//        // SimpleGrantedAuthority 직렬화 지원
-//        objectMapper.registerModules(SecurityJackson2Modules.getModules(getClass().getClassLoader()));
-//        objectMapper.activateDefaultTyping(
-//                BasicPolymorphicTypeValidator.builder()
-//                        .allowIfBaseType(Object.class)
-//                        .build(),
-//                ObjectMapper.DefaultTyping.NON_FINAL
-//        );
-//
-//        // 세션 속성 값을 Java 직렬화 대신 JSON 형태로 저장하도록 설정
-//        return new GenericJackson2JsonRedisSerializer();
-//    }
-
     @Bean
     public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
         return new JdkSerializationRedisSerializer();
